150fb9f86f77ed3f5867e931d53e1a2542904bbc
Majid Farhat <majid.farhat@student.hnu.de>
2024-05-15 12:36:32 +0200

    update

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import logo from '../../assets/logo.png'; // Pfad zum ausgeschnittenen Logo
 
 const Navbar = () => {
   return (
     <header className="navbar">
-      <div className="logo">FabCollab</div>
+      <div className="logo-container">
+        <span className="logo-text">FabCollab</span>
+        <img src={logo} alt="FabCollab Logo" className="logo-image" />
+      </div>
       <nav>
         <Link to="/">Start</Link>
         <Link to="/forum">Forum</Link>
