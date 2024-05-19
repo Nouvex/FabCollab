9ce1b55fc9bf66910de2d9099c99ade288ea8883
Majid Farhat <majid.farhat@student.hnu.de>
2024-05-19 13:43:47 +0200

    update

---
+++ client/src/pages/SubPage/SubPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Forum.css';
+
+const Forum = () => {
+  return (
+    <div className="forum">
+      <h2>Forum</h2>
+      <p>Willkommen im Forum! Diskutieren Sie mit anderen über Gesundheitsthemen.</p>
+    </div>
+  );
+};
+
+export default Forum;
