aec55ed9adabb43c4489ae95608bfd60c549e663
ws22-tom-wagner <Tom.Wagner@student.hnu.de>
2024-05-27 11:41:55 +0200

    dasf

--- client/src/pages/SubPage/SubPage.jsx
+++ client/src/pages/SubPage/SubPage.jsx
@@ -16,20 +16,41 @@ const Forum = () => {
           <img src={therapeut} alt="Therapist" />
         </div>
         <div className="info">
-          <h2>Practice Mustermann for Therapy</h2>
-          <p><strong>Description:</strong> Therapist for Mental Health Problems and Depression</p>
-          <p><strong>Adress:</strong> Musterstraße 1, 12345 Musterstadt</p>
-          <p><strong>Phone Number:</strong> 01234 567890</p>
+          <h2>Praxis Mustermann für Psychische Therapie</h2>
+          <p><strong>Beschreibung:</strong> Therapist for Mental Health Problems and Depression</p>
+          <p><strong>Adresse:</strong> Musterstraße 1, 12345 Musterstadt</p>
+          <p><strong>Telefon:</strong> 01234 567890</p>
           <p><strong>Website:</strong> <a href="https://www.praxiswebsite.de" target="_blank" rel="noopener noreferrer">www.praxiswebsite.de</a></p>
-          <p><strong>Appointment booking:</strong> Online, per Call</p>
+          <p><strong>Terminbuchung:</strong> Online, Telefonisch</p>
         </div>
       </div>
       <div className="description">
+        <h2>Intro</h2>
 <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
      </div>
 
+     <div className="pros-and-cons">
+        <div className="pros">
+          <h3>Pros</h3>
+          <ul>
+            <li><span className="icon">&#x2714;</span> Freundliches Personal</li>
+            <li><span className="icon">&#x2714;</span> Kurze Wartezeiten</li>
+            <li><span className="icon">&#x2714;</span> Zentrale Lage</li>
+            {/* Weitere Pros können hier hinzugefügt werden */}
+          </ul>
+        </div>
+        <div className="cons">
+          <h3>Contras</h3>
+          <ul>
+            <li><span className="icon">&#x2716;</span> Begrenzte Parkmöglichkeiten</li>
+            <li><span className="icon">&#x2716;</span> Hohe Nachfrage</li>
+            {/* Weitere Cons können hier hinzugefügt werden */}
+          </ul>
+        </div>
+      </div>
+
      <div className="insurance-logos">
-        <h2>Supported by:</h2>
+        <h2>Gedeckt von:</h2>
         <div className="logos">
           <img src={aok} alt="AOK" />
           <img src={barmer} alt="Barmer" />
@@ -40,9 +61,14 @@ const Forum = () => {
         </div>
       </div>
 
+      <div className="description">
+        <h2>Spezialisierung</h2>
+<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+     </div>
+
 
       <div className="top-comments">
-        <h2>Top comments from the community</h2>
+        <h2>Top Beiträge aus dem Forum</h2>
         <div className="comments">
           <div className="comment">
             <p>"Dieser Therapeut hat mir sehr geholfen. Sehr empfehlenswert!" - User A</p>
@@ -55,7 +81,7 @@ const Forum = () => {
           </div>
           {/* Weitere Kommentare können hier hinzugefügt werden */}
         </div>
-          <button className="tag-button">Go to Forum</button>
+          <button className="tag-button">Zum Forum</button>
       </div>
     </div>
     </div>
